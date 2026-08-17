b1ade0933a82af02938c328764d0f8d6cef495d2
couchpotatoe1234 <leglerlucas@gmail.com>
2026-08-17 09:06:38 -0700

    fixd music looping bug

--- bones/scripts/GameManager.gd
+++ bones/scripts/GameManager.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var total_coins: int = 0
-var current_level = "res://scenes/how_to_play.tscn"
+var current_level = "res://scenes/Game10.tscn"
 var deaths = 0
 var lives_lost = 0
-var has_double_jump = false 
+var has_double_jump = true 
 var controls_allowed: bool = true
 
 func add_coin() -> void:
--- bones/scripts/credits.gd
+++ bones/scripts/credits.gd
@@ -4,4 +4,5 @@ func _on_button_pressed() -> void:
 	GameManager.lives_lost = 0
 	GameManager.deaths = 0
 	GameManager.total_coins = 0
+	GameManager.current_scene = "res://scenes/how_to_play.tscn"
 	TransitionLayer.change_scene("res://scenes/main_menu.tscn")
